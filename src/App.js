6b4cb2ded6ed8c9fb9ad3ac1a561e77fcda5b507
Sabuj Jana <sabujlovescoding@gmail.com>
2020-02-27 01:37:40 +0530

    .env local added

--- src/App.js
+++ src/App.js
@@ -18,13 +18,17 @@ class App extends React.Component {
   };
 
   async componentDidMount() {
-    console.log("MOUNTED!!!");
+    console.log("MOUNTED!!!", process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
     this.setState({
       loading: true
     });
 
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({
       users: res.data,
