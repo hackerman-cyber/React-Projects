84ad2be1dd882d4bec9443ad83269129723468dc
Sabuj Jana <sabujlovescoding@gmail.com>
2020-02-27 00:06:12 +0530

    convert navbar into rfc. convert static variables also.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PropTypes } from "prop-types";
 
-class Navbar extends Component {
-  static defaultProps = {
-    // default props
-    title: "Github Brad",
-    icons: ["fab", "apple"]
-  };
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <h1>
+        <FontAwesomeIcon icon={props.icons} />
+        {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    // prop typechecking
-    title: PropTypes.string.isRequired,
-    icons: PropTypes.array.isRequired
-  };
+Navbar.defaultProps = {
+  // default props
+  title: "Github Brad",
+  icons: ["fab", "apple"]
+};
 
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <h1>
-          <FontAwesomeIcon icon={this.props.icons} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  // prop typechecking
+  title: PropTypes.string.isRequired,
+  icons: PropTypes.array.isRequired
+};
 
 export default Navbar;
