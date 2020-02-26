c63a8932951958a8d12f3b46e00a5488ee483d1e
Sabuj Jana <sabujlovescoding@gmail.com>
2020-02-27 00:01:19 +0530

    convert stateless userlist to rfc.

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, Button } from "react-bootstrap";
+// stateless component. so better to use react func components
 
-class UserList extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={avatar_url} />
-        <Card.Body>
-          <Card.Title>{login}</Card.Title>
-          <Card.Text />
-          <Button variant="success">
-            <Card.Link href={html_url} style={{ color: "black" }}>
-              Github Link
-            </Card.Link>
-          </Button>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+const UserList = props => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={avatar_url} />
+      <Card.Body>
+        <Card.Title>{login}</Card.Title>
+        <Card.Text />
+        <Button variant="success">
+          <Card.Link href={html_url} style={{ color: "black" }}>
+            Github Link
+          </Card.Link>
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default UserList;
