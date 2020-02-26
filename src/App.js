d07f33b83de1292ec1052c87368b5cf460992b36
Sabuj Jana <sabujlovescoding@gmail.com>
2020-02-26 21:42:10 +0530

    js changes.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./styles.css";
 
 class App extends React.Component {
+  name = "Sabuj";
+  foo = () => {
+    return "JU";
+  };
   render() {
-    return <div className="App" />;
+    const loading = true;
+
+    return (
+      <div className="App">
+        {loading ? <h2>Loading ...</h2> : <h3>No worries welcome </h3>}
+        Hi I am {this.name} and I study in {this.foo()}
+      </div>
+    );
   }
 }
 
